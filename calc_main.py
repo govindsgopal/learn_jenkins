6e13b5a03fdc92cd9cb2b3a75d06694adecab680
govind <govind@gmail.com>
2021-09-06 16:45:01 +0530

    Modified the calc problem

--- calc_main.py
+++ calc_main.py
@@ -1,19 +1,19 @@
 import calc 
-
-while True:
-	calc.calc_menu()
-	ch = input("Enter your choice")
-	a = int(input("Enter the first number"))
-	b = int(input("Enter the second number"))
-	if ch == "1":
-		calc.add(a,b)
-	elif ch == "2":
-		calc.sub(a,b)
-	elif ch == "3":
-		calc.mul(a,b)
-	elif ch == "4":
-		calc.div(a,b)
-	elif ch == "5":
-		break
-	else:
-		print("Invalid input")
+import sys
+#while True:
+#calc.calc_menu()
+ch = sys.argv[1]
+a = int(sys.argv[2])
+b = int(sys.argv[3])
+if ch == "1":
+	calc.add(a,b)
+elif ch == "2":
+	calc.sub(a,b)
+elif ch == "3":
+	calc.mul(a,b)
+elif ch == "4":
+	calc.div(a,b)
+#elif ch == "5":
+#	break
+else:
+	print("Invalid input")
